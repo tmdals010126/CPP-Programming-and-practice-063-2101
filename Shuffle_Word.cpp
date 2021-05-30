38ddc9e7b0d36eac6200b8a90c64937f3d878afc
tmdals010126 <tmdals010126@naver.com>
2021-05-30 18:29:46 +0900

    [ADD] CPP-Project-Code-My-Write

---
+++ Shuffle_Word.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <clocale>  // wstring,wcin,wcout 사용
+#include <ctime>
+#include <random>
+
+using namespace std;
+
+//**********************************************************************************
+//원래 코드에선 구조체로 되어있지만 채점을 위해 분리하다보니 나누어 적게 되었습니다.
+//**********************************************************************************
+vector< vector<wstring>>text; // 2차원 백터를 선언하지만 한글은 string 으로 할시 오류가 발생해서 wstring 으로 선언
+void Shuffle_Word()
+{
+    wstring temp1;              // 단어  임시 저장공간
+    wstring temp2;              // 단어의 뜻 임시 저장공간
+    minstd_rand rand_shuffle(time(nullptr));
+    int n;
+    for (int i = static_cast<int>(text.size()) - 1; i >= 0; i--)
+    {
+        n = rand_shuffle() % text.size();
+        temp1 = text[i][0];
+        text[i][0] = text[n][0];
+        text[n][0] = temp1;
+        temp2 = text[i][1];
+        text[i][1] = text[n][1];
+        text[n][1] = temp2;
+    }
+}
