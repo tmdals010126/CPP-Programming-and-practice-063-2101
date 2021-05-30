fa8387cf784b428fe23992c5d5e2c71c8e322268
tmdals010126 <tmdals010126@naver.com>
2021-05-31 03:12:41 +0900

    [ADD] CPP-Project-Code-My-Write

--- Save_Load.cpp
+++ Save_Load.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -21,167 +22,167 @@ vector< vector<wstring>>review_korean_quiz; // 오답내용 저장
 vector< vector<wstring>>review_english_quiz; // 오답내용 저장
 vector<wstring> temp;         // Eng_Word,Kor_Word 저장, 여러가지단어를 잠시 저장하여 2차원 벡터 배열로 입력할때 사용
 
-void fileRead(wifstream& fin, vector<wstring>& v)
-{
-    wstring line;
+    void fileRead(wifstream& fin, vector<wstring>& v)
+    {
+        wstring line;
 
-    while (getline(fin, line))
-        v.push_back(line);
-}
-void Load_Word() { //오답노트도 읽어오도록 변경
-    wstring Load_Kor, Load_Eng;
-    Load_Kor = L"";
-    Load_Eng = L"";
-    wifstream fin("Word.txt");
-    fin.imbue(std::locale("kor"));//한굴인코딩관련해서 적지않으면 한글이 안적힘
-    int if_find = 0;
-    if (fin.fail()) {
-        cout << "저장된 단어장이 없습니다." << endl;
-        system("pause");
-        return;
+        while (getline(fin, line))
+            v.push_back(line);
     }
-    else {
-        fileRead(fin, load_data);
-        fin.close();
-        for (int i = 0; i < load_data.size(); i++) {
-            for (int j = 0; j < load_data[i].size(); j++) {
-                if (load_data[i][j] == '|') {
-                    if_find = 1;
-                    continue;
-                }
-                else {
-                    if (if_find == 0) {
-                        Load_Eng = Load_Eng + load_data[i][j];
-                    }
-                    if (if_find == 1) {
-                        Load_Kor = Load_Kor + load_data[i][j];
-                    }
-                }
-
-            }
-            if_find = 0;
-            temp.push_back(Load_Eng);
-            temp.push_back(Load_Kor);
-            text.push_back(temp);
-            temp.pop_back();
-            temp.pop_back(); //text 벡터 뒤에 이어 덧붙임
-            Load_Kor = L"";
-            Load_Eng = L""; //초기화
+    void Load_Word() { //오답노트도 읽어오도록 변경
+        wstring Load_Kor, Load_Eng;
+        Load_Kor = L"";
+        Load_Eng = L"";
+        wifstream fin("Word.txt");
+        fin.imbue(std::locale("kor"));//한굴인코딩관련해서 적지않으면 한글이 안적힘
+        int if_find=0;
+        if (fin.fail()) {
+            cout << "저장된 단어장이 없습니다." << endl;
+            system("pause");
+            return;
         }
-    }
-    int num = load_data.size();
-    for (int i = 0; i < num; i++) {
-        load_data.pop_back();//load_data 초기화
-    }
-    wifstream kfin("review_korean_quiz.txt");
-    kfin.imbue(std::locale("kor"));//한굴인코딩관련해서 적지않으면 한글이 안적힘
-    if_find = 0;
-    if (kfin.fail()) {
-        cout << "저장된 단어장이 없습니다." << endl;
-        system("pause");
-        return;
-    }
-    else {
-        fileRead(kfin, load_data);
-        kfin.close();
-        for (int i = 0; i < load_data.size(); i++) {
-            for (int j = 0; j < load_data[i].size(); j++) {
-                if (load_data[i][j] == '|') {
-                    if_find = 1;
-                    continue;
-                }
-                else {
-                    if (if_find == 0) {
-                        Load_Eng = Load_Eng + load_data[i][j];
+        else {
+            fileRead(fin, load_data);
+            fin.close();
+            for (int i = 0; i < load_data.size(); i++) {
+                for (int j = 0; j < load_data[i].size(); j++) {
+                    if (load_data[i][j] == '|') {
+                        if_find = 1;
+                        continue;
                     }
-                    if (if_find == 1) {
-                        Load_Kor = Load_Kor + load_data[i][j];
+                    else {
+                        if (if_find == 0) {
+                            Load_Eng = Load_Eng + load_data[i][j];
+                        }
+                        if (if_find == 1) {
+                            Load_Kor = Load_Kor + load_data[i][j];
+                        }
                     }
-                }
 
+                }
+                if_find = 0;
+                temp.push_back(Load_Eng);
+                temp.push_back(Load_Kor);
+                text.push_back(temp);
+                temp.pop_back();
+                temp.pop_back(); //text 벡터 뒤에 이어 덧붙임
+                Load_Kor = L"";
+                Load_Eng = L""; //초기화
             }
-            if_find = 0;
-            temp.push_back(Load_Eng);
-            temp.push_back(Load_Kor);
-            review_korean_quiz.push_back(temp);
-            temp.pop_back();
-            temp.pop_back(); //text 벡터 뒤에 이어 덧붙임
-            Load_Kor = L"";
-            Load_Eng = L""; //초기화
         }
-    }
-    num = load_data.size();
-    for (int i = 0; i < num; i++) {
-        load_data.pop_back();//load_data 초기화
-    }
-    wifstream efin("review_english_quiz.txt");
-    efin.imbue(std::locale("kor"));//한굴인코딩관련해서 적지않으면 한글이 안적힘
-    if_find = 0;
-    if (efin.fail()) {
-        cout << "저장된 단어장이 없습니다." << endl;
-        system("pause");
-        return;
-    }
-    else {
-        fileRead(efin, load_data);
-        efin.close();
-        for (int i = 0; i < load_data.size(); i++) {
-            for (int j = 0; j < load_data[i].size(); j++) {
-                if (load_data[i][j] == '|') {
-                    if_find = 1;
-                    continue;
-                }
-                else {
-                    if (if_find == 0) {
-                        Load_Eng = Load_Eng + load_data[i][j];
+        int num = load_data.size();
+        for (int i = 0; i < num; i++) {
+            load_data.pop_back();//load_data 초기화
+        }
+        wifstream kfin("review_korean_quiz.txt");
+        kfin.imbue(std::locale("kor"));//한굴인코딩관련해서 적지않으면 한글이 안적힘
+        if_find = 0;
+        if (kfin.fail()) {
+            cout << "저장된 단어장이 없습니다." << endl;
+            system("pause");
+            return;
+        }
+        else {
+            fileRead(kfin, load_data);
+            kfin.close();
+            for (int i = 0; i < load_data.size(); i++) {
+                for (int j = 0; j < load_data[i].size(); j++) {
+                    if (load_data[i][j] == '|') {
+                        if_find = 1;
+                        continue;
                     }
-                    if (if_find == 1) {
-                        Load_Kor = Load_Kor + load_data[i][j];
+                    else {
+                        if (if_find == 0) {
+                            Load_Eng = Load_Eng + load_data[i][j];
+                        }
+                        if (if_find == 1) {
+                            Load_Kor = Load_Kor + load_data[i][j];
+                        }
                     }
-                }
 
+                }
+                if_find = 0;
+                temp.push_back(Load_Eng);
+                temp.push_back(Load_Kor);
+                review_korean_quiz.push_back(temp);
+                temp.pop_back();
+                temp.pop_back(); //text 벡터 뒤에 이어 덧붙임
+                Load_Kor = L"";
+                Load_Eng = L""; //초기화
             }
-            if_find = 0;
-            temp.push_back(Load_Eng);
-            temp.push_back(Load_Kor);
-            review_english_quiz.push_back(temp);
-            temp.pop_back();
-            temp.pop_back(); //text 벡터 뒤에 이어 덧붙임
-            Load_Kor = L"";
-            Load_Eng = L""; //초기화
         }
-        wcout << L"성공적으로 불러왔습니다." << endl;
+        num = load_data.size();
+        for (int i = 0; i < num; i++) {
+            load_data.pop_back();//load_data 초기화
+        }
+        wifstream efin("review_english_quiz.txt");
+        efin.imbue(std::locale("kor"));//한굴인코딩관련해서 적지않으면 한글이 안적힘
+        if_find = 0;
+        if (efin.fail()) {
+            cout << "저장된 단어장이 없습니다." << endl;
+            system("pause");
+            return;
+        }
+        else {
+            fileRead(efin, load_data);
+            efin.close();
+            for (int i = 0; i < load_data.size(); i++) {
+                for (int j = 0; j < load_data[i].size(); j++) {
+                    if (load_data[i][j] == '|') {
+                        if_find = 1;
+                        continue;
+                    }
+                    else {
+                        if (if_find == 0) {
+                            Load_Eng = Load_Eng + load_data[i][j];
+                        }
+                        if (if_find == 1) {
+                            Load_Kor = Load_Kor + load_data[i][j];
+                        }
+                    }
+
+                }
+                if_find = 0;
+                temp.push_back(Load_Eng);
+                temp.push_back(Load_Kor);
+                review_english_quiz.push_back(temp);
+                temp.pop_back();
+                temp.pop_back(); //text 벡터 뒤에 이어 덧붙임
+                Load_Kor = L"";
+                Load_Eng = L""; //초기화
+            }
+            wcout << L"성공적으로 불러왔습니다." << endl;
+            system("pause");
+            return;
+        }
+
+    }
+
+    void Save_Word() { //오답노트도 백업하도록 변경
+        wstring Save_data;
+
+        wofstream fout("Word.txt",wios::out);
+        fout.imbue(std::locale("kor")); //한굴인코딩관련해서 이 코드를 적지않으면 한글이 안적힘
+        for (int i = 0; i < text.size(); i++) {
+            Save_data = text[i][0] + L"|" + text[i][1];
+            fout << Save_data << endl;
+        }
+        fout.close();
+        wofstream kfout("review_korean_quiz.txt", wios::out);
+        kfout.imbue(std::locale("kor")); //한굴인코딩관련해서 이 코드를 적지않으면 한글이 안적힘
+        for (int i = 0; i < review_korean_quiz.size(); i++) {
+            Save_data = review_korean_quiz[i][0] + L"|" + review_korean_quiz[i][1];
+            kfout << Save_data << endl;
+        }
+        kfout.close();
+        wofstream efout("review_english_quiz.txt", wios::out);
+        efout.imbue(std::locale("kor")); //한굴인코딩관련해서 이 코드를 적지않으면 한글이 안적힘
+        for (int i = 0; i < review_english_quiz.size(); i++) {
+            Save_data = review_english_quiz[i][0] + L"|" + review_english_quiz[i][1];
+            efout << Save_data << endl;
+        }
+        efout.close();
+        wcout << L"성공적으로 저장했습니다." << endl;
         system("pause");
         return;
     }
-
-}
-
-void Save_Word() { //오답노트도 백업하도록 변경
-    wstring Save_data;
-
-    wofstream fout("Word.txt", wios::out);
-    fout.imbue(std::locale("kor")); //한굴인코딩관련해서 이 코드를 적지않으면 한글이 안적힘
-    for (int i = 0; i < text.size(); i++) {
-        Save_data = text[i][0] + L"|" + text[i][1];
-        fout << Save_data << endl;
-    }
-    fout.close();
-    wofstream kfout("review_korean_quiz.txt", wios::out);
-    kfout.imbue(std::locale("kor")); //한굴인코딩관련해서 이 코드를 적지않으면 한글이 안적힘
-    for (int i = 0; i < review_korean_quiz.size(); i++) {
-        Save_data = review_korean_quiz[i][0] + L"|" + review_korean_quiz[i][1];
-        kfout << Save_data << endl;
-    }
-    kfout.close();
-    wofstream efout("review_english_quiz.txt", wios::out);
-    efout.imbue(std::locale("kor")); //한굴인코딩관련해서 이 코드를 적지않으면 한글이 안적힘
-    for (int i = 0; i < review_english_quiz.size(); i++) {
-        Save_data = review_english_quiz[i][0] + L"|" + review_english_quiz[i][1];
-        efout << Save_data << endl;
-    }
-    efout.close();
-    wcout << L"성공적으로 저장했습니다." << endl;
-    system("pause");
-    return;
-}
